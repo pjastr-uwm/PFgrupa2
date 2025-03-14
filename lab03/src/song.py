4975db1f8d16785101bfba566f56bce3406f9322
pjastr <114875756+pjastr-uwm@users.noreply.github.com>
2025-03-14 10:36:07 +0100

    etap 4

--- lab03/src/song.py
+++ lab03/src/song.py
@@ -3,3 +3,6 @@ class Song:
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
+
+    def calculate_royalty(self):
+        return 0.1 * self.duration
--- lab03/tests/test_song.py
+++ lab03/tests/test_song.py
@@ -15,6 +15,12 @@ class TestSongInitialization(unittest.TestCase):
         self.assertEqual(song1.title, "Bohemian Rhapsody")
         self.assertEqual(song1.duration, 355)
 
+    def test_calculate_royalty(self):
+        song1 = Song("Bohemian Rhapsody", 355)
+        self.assertAlmostEqual(song1.calculate_royalty(), 35.5)
+        song2 = Song("Yesterday", 125)
+        self.assertAlmostEqual(song2.calculate_royalty(), 12.5)
+
     def tearDown(self):
         pass
 
